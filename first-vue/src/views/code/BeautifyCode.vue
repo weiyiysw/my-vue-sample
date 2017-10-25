dd92db224cc7e0ae92d8a415601270dd97489fb5
ysw <ysw@ciphergateway.com>
2017-10-25 08:56:43 +0800

    js-beautify美化页面上的代码块

---
+++ first-vue/src/views/code/BeautifyCode.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <span>未格式化</span>
+    <div style="text-align: left;">
+      <pre v-highlightjs="data"><code></code></pre>
+    </div>
+
+    <span>
+      计算属性实现格式化
+    </span>
+    <div style="text-align: left;">
+      <pre v-highlightjs="formatData">
+        <code></code>
+      </pre>
+    </div>
+
+    <span>
+      <el-button @click="formatCode">点击按钮实现格式化</el-button>
+    </span>
+    <div style="text-align: left;">
+      <pre v-highlightjs="fdata">
+        <code></code>
+      </pre>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BeautifyCode',
+  data() {
+    return {
+      data: 'function(test) {console.log(test)}',
+      fdata: ''
+    }
+  },
+  computed: {
+    formatData: function() {
+      const f = this.$jsBeautify(this.data, { indent_size: 2})
+      return f
+    }
+  },
+  methods: {
+    formatCode() {
+      this.fdata = this.$jsBeautify(this.data, { indent_size: 2})
+      console.log(this.fdata)
+    }
+  }
+}
+</script>
---
+++ first-vue/src/views/code/HighlightCode.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="content-text">
+  <pre v-highlightjs><code>// Static source code
+function(test) {
+    console.log(test)
+}</code></pre>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'highlightCode'
+}
+</script>
+
+<style scoped>
+.content-text {
+  text-align: left;
+}
+</style>
